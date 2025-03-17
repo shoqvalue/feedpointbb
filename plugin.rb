73dc1a5dc6022f21b82b4b1c65b40d6c90d2b8a6
shoqvalue <shoqvalue@gmail.com>
2025-03-17 18:29:36 -0400

    Fixed plugin.rb metadata so Discourse can recognize the plugin

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# name: Feed Point Bulletin Board Pluginbu
-# about: A plugin to organize posts into bulletin folders in Discourse
-# version: 0.1
-# authors: Matthew R. Edelstein, Fort Lauderdale, FL
-# url: https://github.com/yourusername/feedpointbb
+name: Feed Point Bulletin Board Plugin
+about: A plugin to organize posts into bulletin folders in Discourse
+version: 0.1
+authors: Matthew R. Edelstein, Fort Lauderdale, FL
+url: https://github.com/shoqvalue/feedpointbb
 
 enabled_site_setting :folders_enabled
